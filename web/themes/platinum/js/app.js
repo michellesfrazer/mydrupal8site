de9d4d2c2a9fcba25ec583312d2ea3a90dd0068e
michelle <msoutherlandfrazer@gmail.com>
2020-08-16 16:27:38 -0400

    New files for new D8 site

---
+++ web/themes/platinum/js/app.js
@@ -0,0 +1,12 @@
+(function ($, Drupal, window, document, undefined) {
+
+
+})(jQuery, Drupal, this, this.document);
+
+(function($) {
+Drupal.behaviors.myBehavior = {
+  attach: function (context, settings) {
+
+
+}};
+})(jQuery);
